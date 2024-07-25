0814889889b3225e259ab4225c5f17d4b04c2a30
PEZI456 <ppezi456@gmail.com>
2024-07-25 13:13:44 +0100

    Update metamask_bf_v1.py

--- metamask_bf_v1.py
+++ metamask_bf_v1.py
@@ -36,7 +36,7 @@ seed_words_test = {1:'abandon',
 12:'accident'}
 
 
-EXTENSION_PATH = "" #enter the path to the metamask .crx file here
+EXTENSION_PATH = "" #nkbihfbeogaeaoehlefnkodbefgpgknn
 
 opt = webdriver.ChromeOptions()
 opt.add_extension(EXTENSION_PATH)
